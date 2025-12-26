ab09950c25390c006485b5552b52c318780b8c41
송하민 <russell@wemade.com>
2025-12-26 11:00:25 +0900

    미사용 코드 제거: dummyData

--- shared/src/commonMain/kotlin/com/wemade/kmp/rocket/model/DetailData.kt
+++ shared/src/commonMain/kotlin/com/wemade/kmp/rocket/model/DetailData.kt
@@ -18,21 +18,3 @@ data class DetailData(
     val images: List<String>,
     val wikipedia: String
 )
-
-//// TODO: 삭제 - 더미 데이터
-//val dummyDetailData = DetailData(
-//    id = "id_0",
-//    rocket = "test_0",
-//    title = "Rocket 1",
-//    description = "Description for Rocket",
-//    createdAt = "2025-12-01 15:11:00",
-//    imageUrl = "https://picsum.photos/id/1/100/140",
-//    isSuccessLaunched = Random.nextBoolean(),
-//    height = 123.0,
-//    diameter = 456.0,
-//    mass = 789.0,
-//    images = List(size = 3) { index ->
-//        "https://picsum.photos/id/${index + 1}/100/140"
-//    },
-//    wikipedia = "Wiki"
-//)

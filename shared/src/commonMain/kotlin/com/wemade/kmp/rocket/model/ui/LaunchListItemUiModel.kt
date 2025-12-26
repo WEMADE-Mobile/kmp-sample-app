cc88fbede024eab7db22bb1bd9b0e5a5de778d82
Ada <dhgang@wemade.com>
2025-12-26 19:57:07 +0900

    feat: 필요 없어진 viewModel 관련 클래스 삭제

--- shared/src/commonMain/kotlin/com/wemade/kmp/rocket/model/ui/LaunchListItemUiModel.kt
+++
@@ -1,9 +0,0 @@
-package com.wemade.kmp.rocket.model.ui
-
-data class LaunchListItemUiModel(
-    val id: String,
-    val imageUrl: String?,
-    val statusText: String,
-    val name: String,
-    val launchDate: String
-)
